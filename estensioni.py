e324cb2d7b147346489a95966a82f12323e9904b
Andex125 <andreaaivaliotis@gmail.com>
2025-02-01 03:20:40 +0100

    aggiunte le raccomandazioni alla base di dati

--- estensioni.py
+++ estensioni.py
@@ -5,53 +5,73 @@ import os
 import pandas as pd
 import torch_geometric.transforms as T
 import torch.nn.functional as F
-from torch import Tensor
+from torch import Tensor, cosine_similarity
 from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_score
 from torch_geometric.data import download_url, extract_zip, HeteroData
 from torch_geometric.loader import LinkNeighborLoader
 from torch_geometric.nn import SAGEConv, to_hetero
 
 
-#------------------------ fine blocco 0 e inzio blocco 1
-def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors, neg_sampling_ratio, excel_filename):
+# Definizione delle classi
+class GNN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.conv1 = SAGEConv(hidden_channels, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels)
 
-    print(torch.__version__)
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.conv2(x, edge_index)
+        return x
 
-    #------------------------ fine blocco 1 e inzio blocco 2
-    #
+class Classifier(torch.nn.Module):
+    def forward(self, x_user: Tensor, x_movie: Tensor, edge_label_index: Tensor) -> Tensor:
+        edge_feat_user = x_user[edge_label_index[0]]
+        edge_feat_movie = x_movie[edge_label_index[1]]
+        return (edge_feat_user * edge_feat_movie).sum(dim=-1)
+
+class Model(torch.nn.Module):
+    def __init__(self, hidden_channels, data):
+        super().__init__()
+        self.movie_lin = torch.nn.Linear(20, hidden_channels)
+        self.user_emb = torch.nn.Embedding(data["user"].num_nodes, hidden_channels)
+        self.movie_emb = torch.nn.Embedding(data["movie"].num_nodes, hidden_channels)
+        self.gnn = GNN(hidden_channels)
+        self.gnn = to_hetero(self.gnn, metadata=data.metadata())
+        self.classifier = Classifier()
+
+    def forward(self, data: HeteroData) -> Tensor:
+        x_dict = {
+            "user": self.user_emb(data["user"].node_id),
+            "movie": self.movie_lin(data["movie"].x) + self.movie_emb(data["movie"].node_id),
+        }
+        x_dict = self.gnn(x_dict, data.edge_index_dict)
+        pred = self.classifier(
+            x_dict["user"],
+            x_dict["movie"],
+            data["user", "rates", "movie"].edge_label_index,
+        )
+        return pred
+
+def prepare_data():
+
     os.environ['TORCH'] = torch.__version__
-
+    
     url = 'https://files.grouplens.org/datasets/movielens/ml-latest-small.zip'
     extract_zip(download_url(url, '.'), '.')
-
+    
     movies_path = './ml-latest-small/movies.csv'
     ratings_path = './ml-latest-small/ratings.csv'
-
-    #------------------------ fine blocco 2 e inzio blocco 3
-
-
-    print('movies.csv:')
-    print('===========')
-    print(pd.read_csv(movies_path)[["movieId", "genres"]].head())
-    print()
-    print('ratings.csv:')
-    print('============')
-    print(pd.read_csv(ratings_path)[["userId", "movieId"]].head())
-
-    #------------------------ fine blocco 3 e inzio blocco 4
-
+    
     # Load the entire movie data frame into memory:
     movies_df = pd.read_csv(movies_path, index_col='movieId')
 
     # Split genres and convert into indicator variables:
     genres = movies_df['genres'].str.get_dummies('|')
-    print(genres[["Action", "Adventure", "Drama", "Horror"]].head())
 
     # Use genres as movie input features:
     movie_feat = torch.from_numpy(genres.values).to(torch.float)
     assert movie_feat.size() == (9742, 20)  # 20 genres in total.
-
-    #------------------------ fine blocco 4 e inzio blocco 5
 
     # Load the entire ratings data frame into memory:
     ratings_df = pd.read_csv(ratings_path)
@@ -62,19 +82,11 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
         'userId': unique_user_id,
         'mappedID': pd.RangeIndex(len(unique_user_id)),
     })
-    print("Mapping of user IDs to consecutive values:")
-    print("==========================================")
-    print(unique_user_id.head())
-    print()
     # Create a mapping from unique movie indices to range [0, num_movie_nodes):
     unique_movie_id = pd.DataFrame(data={
         'movieId': movies_df.index,
         'mappedID': pd.RangeIndex(len(movies_df)),
     })
-    print("Mapping of movie IDs to consecutive values:")
-    print("===========================================")
-    print(unique_movie_id.head())
-
     # Perform merge to obtain the edges from users and movies:
     ratings_user_id = pd.merge(ratings_df['userId'], unique_user_id,
                                 left_on='userId', right_on='userId', how='left')
@@ -88,38 +100,16 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
     edge_index_user_to_movie = torch.stack([ratings_user_id, ratings_movie_id], dim=0)
     assert edge_index_user_to_movie.size() == (2, 100836)
 
-    print()
-    print("Final edge indices pointing from users to movies:")
-    print("=================================================")
-    print(edge_index_user_to_movie)
-
-    #------------------------ fine blocco 5 e inzio blocco 6
-
-
-    
-
     data = HeteroData()
 
     # Save node indices:
     data["user"].node_id = torch.arange(len(unique_user_id))
     data["movie"].node_id = torch.arange(len(movies_df))
 
-    # Add the node features and edge indices:
-    #data["movie"].x = ...  # TODO
-    #data["user", "rates", "movie"].edge_index = ...  # TODO
     data["movie"].x = movie_feat
     data["user", "rates", "movie"].edge_index = edge_index_user_to_movie
 
-    # We also need to make sure to add the reverse edges from movies to users
-    # in order to let a GNN be able to pass messages in both directions.
-    # We can leverage the T.ToUndirected() transform for this from PyG:
-
-    # TODO:
-    #raise NotImplementedError
     data = T.ToUndirected()(data)
-
-    print(data)
-
     assert data.node_types == ["user", "movie"]
     assert data.edge_types == [("user", "rates", "movie"),
                             ("movie", "rev_rates", "user")]
@@ -130,16 +120,6 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
     assert data["user", "rates", "movie"].num_edges == 100836
     assert data["movie", "rev_rates", "user"].num_edges == 100836
 
-    #---------------- fine blocco 6 e inzio blocco 7
-
-    # For this, we first split the set of edges into
-    # training (80%), validation (10%), and testing edges (10%).
-    # Across the training edges, we use 70% of edges for message passing,
-    # and 30% of edges for supervision.
-    # We further want to generate fixed negative edges for evaluation with a ratio of 2:1.
-    # Negative edges during training will be generated on-the-fly, so we don't want to
-    # add them to the graph right away.
-    # Overall, we can leverage the RandomLinkSplit() transform for this from PyG:
     transform = T.RandomLinkSplit(
         num_val=0.1,
         num_test=0.1,
@@ -148,23 +128,9 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
         add_negative_train_samples=False,
         edge_types=("user", "rates", "movie"),
         rev_edge_types=("movie", "rev_rates", "user"),
-        #num_val=...,  # TODO
-        #num_test=...,  # TODO
-        #disjoint_train_ratio=...,  # TODO
-        #neg_sampling_ratio=...,  # TODO
-        #add_negative_train_samples=...,  # TODO
-        #edge_types=("user", "rates", "movie"),
-        #rev_edge_types=("movie", "rev_rates", "user"),
     )
 
     train_data, val_data, test_data = transform(data)
-    print("Training data:")
-    print("==============")
-    print(train_data)
-    print()
-    print("Validation data:")
-    print("================")
-    print(val_data)
 
     assert train_data["user", "rates", "movie"].num_edges == 56469
     assert train_data["user", "rates", "movie"].edge_label_index.size(1) == 24201
@@ -179,7 +145,21 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
     # Negative edges with ratio 2:1:
     assert val_data["user", "rates", "movie"].edge_label.long().bincount().tolist() == [20166, 10083]
 
-    #---------------- fine blocco 7 e inzio blocco 8
+    return train_data, val_data, test_data
+
+# Funzione per estrarre embedding e fare Collaborative Filtering
+def recommend_movies(user_embeddings, movie_embeddings, top_k=5):
+    similarity_matrix = F.cosine_similarity(user_embeddings.unsqueeze(1), 
+                                        movie_embeddings.unsqueeze(0), dim=-1)
+    recommendations = {}
+    for user_idx in range(len(user_embeddings)):
+        recommended_movie_indices = similarity_matrix[user_idx].argsort(descending=True)[:top_k]
+        recommendations[user_idx] = recommended_movie_indices.tolist()
+    return recommendations
+
+#------------------------ fine blocco 0 e inzio blocco 1
+def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors, neg_sampling_ratio, excel_filename):
+    train_data, val_data, test_data = prepare_data()
 
     # In the first hop, we sample at most 20 neighbors.
     # In the second hop, we sample at most 10 neighbors.
@@ -193,9 +173,6 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
     edge_label = train_data["user", "rates", "movie"].edge_label
 
     train_loader = LinkNeighborLoader(
-        #data=...,  # TODO
-        #num_neighbors=...,  # TODO
-        #neg_sampling_ratio=...,  # TODO
         data=train_data,
         num_neighbors=num_neighbors,
         neg_sampling_ratio=neg_sampling_ratio,
@@ -208,89 +185,8 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
     # Inspect a sample:
     sampled_data = next(iter(train_loader))
 
-    print("Sampled mini-batch:")
-    print("===================")
-    print(sampled_data)
-
-    
-   #---------------- fine blocco 8 e inzio blocco 9
-    
-
-    class GNN(torch.nn.Module):
-        def __init__(self, hidden_channels):
-            super().__init__()
-
-            self.conv1 = SAGEConv(hidden_channels, hidden_channels)
-            self.conv2 = SAGEConv(hidden_channels, hidden_channels)
-
-        def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
-            # Define a 2-layer GNN computation graph.
-            # Use a *single* ReLU non-linearity in-between.
-            # TODO:
-            #raise NotImplementedError
-            x = F.relu(self.conv1(x, edge_index))
-            x = self.conv2(x, edge_index)
-            return x
-
-    # Our final classifier applies the dot-product between source and destination
-    # node embeddings to derive edge-level predictions:
-    class Classifier(torch.nn.Module):
-        def forward(self, x_user: Tensor, x_movie: Tensor, edge_label_index: Tensor) -> Tensor:
-            # Convert node embeddings to edge-level representations:
-            edge_feat_user = x_user[edge_label_index[0]]
-            edge_feat_movie = x_movie[edge_label_index[1]]
-
-            # Apply dot-product to get a prediction per supervision edge:
-            return (edge_feat_user * edge_feat_movie).sum(dim=-1)
-
-
-    class Model(torch.nn.Module):
-        def __init__(self, hidden_channels):
-            super().__init__()
-            # Since the dataset does not come with rich features, we also learn two
-            # embedding matrices for users and movies:
-            self.movie_lin = torch.nn.Linear(20, hidden_channels)
-            self.user_emb = torch.nn.Embedding(data["user"].num_nodes, hidden_channels)
-            self.movie_emb = torch.nn.Embedding(data["movie"].num_nodes, hidden_channels)
-
-            # Instantiate homogeneous GNN:
-            self.gnn = GNN(hidden_channels)
-
-            # Convert GNN model into a heterogeneous variant:
-            self.gnn = to_hetero(self.gnn, metadata=data.metadata())
-
-            self.classifier = Classifier()
-
-        def forward(self, data: HeteroData) -> Tensor:
-            x_dict = {
-            "user": self.user_emb(data["user"].node_id),
-            "movie": self.movie_lin(data["movie"].x) + self.movie_emb(data["movie"].node_id),
-            }
-
-            # x_dict holds feature matrices of all node types
-            # edge_index_dict holds all edge indices of all edge types
-            x_dict = self.gnn(x_dict, data.edge_index_dict)
-
-            pred = self.classifier(
-                x_dict["user"],
-                x_dict["movie"],
-                data["user", "rates", "movie"].edge_label_index,
-            )
-
-            return pred
-
-
-    model = Model(hidden_channels=hidden_channels)
-
-    print(model)
-
-    #---------------- fine blocco 9 e inzio blocco 10
-
-    
-
+    model = Model(hidden_channels=hidden_channels , data=train_data)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"Device: '{device}'")
-
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -299,28 +195,16 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
         total_loss = total_examples = 0
         for sampled_data in tqdm.tqdm(train_loader):
             optimizer.zero_grad()
-
-            # TODO: Move sampled_data to the respective device
-            # TODO: Run forward pass of the model
-            # TODO: Apply binary cross entropy via
-            # F.binary_cross_entropy_with_logits(pred, ground_truth)
-            # raise NotImplementedError
-
             sampled_data.to(device)
             pred = model(sampled_data)
-
             ground_truth = sampled_data["user", "rates", "movie"].edge_label
             loss = F.binary_cross_entropy_with_logits(pred, ground_truth)
-
-
             loss.backward()
             optimizer.step()
             total_loss += float(loss) * pred.numel()
             total_examples += pred.numel()
             loss = total_loss / total_examples
         print(f"Epoch: {epoch:03d}, Loss: {total_loss / total_examples:.4f}")
-
-    #---------------- fine blocco 10 e inzio blocco 11
 
     # Define the validation seed edges:
     edge_label_index = val_data["user", "rates", "movie"].edge_label_index
@@ -337,26 +221,16 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
 
     sampled_data = next(iter(val_loader))
 
-    print("Sampled mini-batch:")
-    print("===================")
-    print(sampled_data)
 
     assert sampled_data["user", "rates", "movie"].edge_label_index.size(1) == 3 * 128
     assert sampled_data["user", "rates", "movie"].edge_label.min() >= 0
     assert sampled_data["user", "rates", "movie"].edge_label.max() <= 1
-
-    #---------------- fine blocco 11 e inzio blocco 12
-
-    
 
     preds = []
     ground_truths = []
     total_val_loss = total_val_examples = 0 
     for sampled_data in tqdm.tqdm(val_loader):
         with torch.no_grad():
-            # TODO: Collect predictions and ground-truths and write them into
-            # preds and ground_truths.
-            # raise NotImplementedError
             sampled_data.to(device)
             val_loss = F.binary_cross_entropy_with_logits(pred, ground_truth)
             total_val_loss += float(val_loss) * pred.numel()
@@ -413,6 +287,47 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
     df.to_excel(excel_filename, index=False)
     print(f"Risultati salvati in {excel_filename}")
     
+    # Estrai gli embedding degli utenti e dei film
+    user_embeddings = model.user_emb.weight.data
+    movie_embeddings = model.movie_emb.weight.data
+
+    # Genera raccomandazioni per ogni utente
+    recommendations = recommend_movies(user_embeddings, movie_embeddings, top_k=5)
+
+    # Simuliamo che l'utente accetti alcuni consigli (prendiamo i primo film suggeriti per ogni utente)
+    simulated_feedback = {user: recs[:1] for user, recs in recommendations.items()}
+
+    MAX_HISTORY = 5  # Numero massimo di feedback per utente
+
+    # Dizionario per tenere traccia delle interazioni utente-film già presenti
+    user_feedback = {}
+
+    # Recupera le interazioni esistenti nel dataset
+    existing_edges = train_data["user", "rates", "movie"].edge_index.cpu().T.tolist()
+
+    # Popola il dizionario con le interazioni esistenti
+    for user, movie in existing_edges:
+        if user not in user_feedback:
+            user_feedback[user] = []
+        user_feedback[user].append(movie)
+
+    # Aggiunge solo le nuove interazioni, mantenendo al massimo MAX_HISTORY per utente
+    for user, movies in simulated_feedback.items():
+        if user not in user_feedback:
+            user_feedback[user] = []
+        
+        user_feedback[user].extend(movies)  # Aggiunge i nuovi film consigliati
+        user_feedback[user] = user_feedback[user][-MAX_HISTORY:]  # Mantiene solo gli ultimi 5 film
+
+    # Convertiamo il nuovo set di interazioni in tensore
+    filtered_edges = [[user, movie] for user, movies in user_feedback.items() for movie in movies]
+    filtered_edges = torch.tensor(filtered_edges).T  # Shape (2, num_interazioni_filtrate)
+
+    # Aggiorna il dataset con le nuove interazioni "filtrate"
+    train_data["user", "rates", "movie"].edge_index = filtered_edges
+
+    print(f"Aggiunte {additional_edges.size(1)} nuove interazioni utente-film simulate.")
+
 
     
     #---------------- fine blocco 12 e inzio blocco 13
@@ -420,6 +335,7 @@ def train_and_evaluate(hidden_channels, learning_rate, batch_size, num_neighbors
 
 # codice che esegue la funzione train_and_evaluate per ogni valore di iperpametri con cicli for
 # e salva i risultati su un file excel
+
 # Creazione del file Excel
 excel_filename = "results.xlsx"
 df = pd.DataFrame(columns=["Hidden Channels", "Learning Rate", "Batch Size", "Num Neighbors", "Neg Sampling Ratio", "AUC", "F1-score", "Precision", "Recall", "Loss"])
@@ -431,13 +347,6 @@ learning_rates = [0.01, 0.005, 0.001, 0.0005]
 batch_sizes = [64, 128, 256]
 num_neighbors_list = [[10, 5], [20, 10], [30, 15]]
 neg_sampling_ratios = [1.0, 2.0, 3.0]
-
-#testing se funzionava
-#hidden_channels_list = [32]
-#learning_rates = [0.01]
-#batch_sizes = [64]
-#num_neighbors_list = [[10, 5]]
-#neg_sampling_ratios = [1.0, 2.0, 3.0]
 
 for hidden_channels in hidden_channels_list:
     for learning_rate in learning_rates:
